49e0d829fadec9a3551bc9dbb396403e12bcbc3f
ATN04 <95452323+EtienneK2004@users.noreply.github.com>
2022-07-20 18:41:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,19 +9,30 @@ def relative_to_absolute(baseDir, relativeDir):
     dirs = ['North', 'East', 'South', 'West']
     return dirs[(dirs.index(baseDir) + dirs.index(relativeDir))%4]
 
+
+def rotate_block(Blockname, dir):
+
+    return Blockname.split('_')[0] + '_' + relative_to_absolute(Blockname.split('_')[1], dir)
+
+
+
+
+
 class Block:
     name = ""
-    # This lists contain the blocks' names
-    relativeNorth = []
-    relativeEast = []
-    relativeSouth = []
-    relativeWest = []
-    def __init__(self, bDict):
-        self.name = bDict['name']
-        self.relativeNorth = bDict['north']
-        self.relativeEast = bDict['east']
-        self.relativeSouth = bDict['south']
-        self.relativeWest = bDict['west']
+    possibleBlocks = {}
+    def __init__(self, bDict, rotation):
+        dirs = ['North', 'East', 'South', 'West']
+        self.name = bDict['name'] + "_" + rotation
+        self.possibleBlocks[relative_to_absolute("North", rotation)] = bDict['north']
+        self.possibleBlocks[relative_to_absolute("East", rotation)] = bDict['east']
+        self.possibleBlocks[relative_to_absolute("South", rotation)] = bDict['south']
+        self.possibleBlocks[relative_to_absolute("West", rotation)] = bDict['west']
+
+        for d in dirs:
+            for i in range(len(self.possibleBlocks[d])):
+                self.possibleBlocks[d][i] = rotate_block(self.possibleBlocks[d][i], rotation)
+
 
 
 
@@ -34,25 +45,29 @@ class BlockSet:
         self.name = BlockSetJson['name']
         self.dimensions = BlockSetJson['dimensions']
         for b in BlockSetJson['blocks']:
-            self.listBlocks[b['name']] = Block(b)
+            dirs = ['North', 'East', 'South', 'West']
+            for d in dirs:
+                self.listBlocks[b['name']+'_'+d] = Block(b, d)
 
     def get_all_blocks(self):
         return self.listBlocks.values()
 
-    def get_all_blocks_all_dir(self):
+
+    def get_all_blocknames(self):
         allblocks = self.get_all_blocks()
-        allblocksalldir = []
+        allblocknames = []
         for block in allblocks:
-            allblocksalldir.append(block.name + "_North")
-            allblocksalldir.append(block.name + "_East")
-            allblocksalldir.append(block.name + "_South")
-            allblocksalldir.append(block.name + "_West")
-        return allblocksalldir
+            allblocknames.append(block.name)
+        return allblocknames
+
+
 
     def possible_blocks_near(self, blockname, dir, basedir=None):
         if basedir is None:
-            return self.listBlocks[blockname][dir]
-        return self.listBlocks[blockname][relative_to_absolute(basedir, dir)]
+            return self.listBlocks[blockname].possibleBlocks[dir]
+        return list(map(lambda x : rotate_block(x, dir), self.listBlocks[blockname][relative_to_absolute(basedir, dir)]))
+
+
 
 
     def get_name(self):
@@ -71,10 +86,14 @@ class Stadium:
         if BlockSet.dimensions == 2:
             for x in range(42):
                 for z in range(42):
-                    self.add_tile((x, Y_AXIS_FOR_2D, z), BlockSet)
+                    self.add_tile((x, Y_AXIS_FOR_2D, z))
 
-    def add_tile(self, coords3D, BlockSet):
-        self.tiles[coords3D] = Tile(BlockSet.get_all_blocks_all_dir())
+    def add_tile(self, coords3D):
+        self.tiles[coords3D] = Tile(self.blockSet.get_all_blocknames())
+
+
+    def is_coords_out_of_stadium(self, coords):
+        return coords[0] > 41 or coords[1] > 31 or coords[2] > 41 or coords[0] < 0 or coords[1] < 9 or coords[2] < 0
 
 
     def get_superpositions(self, coords3D):
@@ -85,14 +104,74 @@ class Stadium:
         self.tiles[coords3D].force_collapse(superposition)
 
 
+    def neighbours_of_coords(self, coords3D, dimensions=3):
+        listn = []
+        listn.append((coords3D[0]+1,coords3D[1], coords3D[2]))
+        listn.append((coords3D[0]-1,coords3D[1], coords3D[2]))
+        if dimensions > 1:
+            listn.append((coords3D[0],coords3D[1], coords3D[2]+1))
+            listn.append((coords3D[0],coords3D[1], coords3D[2]-1))
+        if dimensions > 2:
+            listn.append((coords3D[0],coords3D[1]+1, coords3D[2]))
+            listn.append((coords3D[0],coords3D[1]-1, coords3D[2]))
+
+        rmv = []
+
+        for i in range(len(listn)):
+            if self.is_coords_out_of_stadium(listn[i]):
+                rmv.append(listn[i])
+
+        for i in rmv:
+            listn.remove(i)
+
+        return listn
+
+
+    def can_Block_be_at_Direction_of_Tile(self, blockname, dir, coords):
+        if self.is_coords_out_of_stadium(coords):
+            return True
+
+        for b in self.tiles[coords].get_superpositions():
+            print('comparaison', blockname, self.blockSet.possible_blocks_near(b, dir))
+            if blockname in self.blockSet.possible_blocks_near(b, dir):
+                return True
+
+        return False
+
+
     def refresh_tile(self, coords3D):
+        print('refresh', coords3D)
         # For each superposition, keep it if all neighbours have a superposition allowing it
         # If a change was made, refresh neighbours
+        dirs = ['East', 'West', "South",'North']
+        keeps = []
+        change = False
+        for superpos in self.tiles[coords3D].get_superpositions():
+            keep = True
+            idir = 0
+            for n in self.neighbours_of_coords(coords3D, dimensions=2):
+                if not self.can_Block_be_at_Direction_of_Tile(superpos, dirs[idir], n):
+                    keep = False
+                    change = True
+                    break
 
-        for superpos in self.tiles[coords3D]:
+
+                idir = (idir+1)%4
+
+            if keep:
+                keeps.append(superpos)
+        if change:
+            print()
+            print(keeps)
+            self.tiles[coords3D].reset_superpositions(newSuperpositions=keeps)
+            print(coords3D, self.tiles[coords3D].get_superpositions())
+            for n in self.neighbours_of_coords(coords3D, dimensions=2):
+                self.refresh_tile(n)
 
 
-        pass
+
+
+
         # VERY IMPORTANT
 
 
@@ -101,7 +180,7 @@ class Stadium:
         min = len(self.blockSet.get_all_blocks())
         coordsofmin = None
         for coords in self.tiles.keys():
-            if self.tiles[coords].nb_superpositions() < min:
+            if not self.tiles[coords].isCollapse() and self.tiles[coords].nb_superpositions() < min:
                 min = self.tiles[coords].nb_superpositions()
                 coordsofmin = coords
 
@@ -120,6 +199,20 @@ class Stadium:
         return json.dumps(list)
 
 
+    def solve(self):
+        co = list(self.tiles.keys())[random.randint(0, len(self.tiles.keys())-1)]
+
+
+        while co is not None:
+            print(co)
+            self.collapse(co)
+            for n in self.neighbours_of_coords(co, dimensions=2):
+                self.refresh_tile(n)
+            co = self.find_lowest_superposition()
+
+
+
+
 
 
 class Tile:
@@ -128,7 +221,7 @@ class Tile:
 
 
     def __init__(self, blocks):
-        self.superpositions = blocks
+        self.superpositions = list(blocks)
 
 
     def get_superpositions(self):
@@ -137,16 +230,19 @@ class Tile:
     def nb_superpositions(self):
         return len(self.superpositions)
 
-    def reset_superpositions(self):
+    def reset_superpositions(self, newSuperpositions=None):
         s = self.superpositions
-        self.superpositions = []
+        if newSuperpositions is None:
+            self.superpositions = []
+        else:
+            self.superpositions = newSuperpositions
         return s
 
 
 
 
     def isCollapse(self):
-        return len(self.superpositions) == 1
+        return self.collapse is not None
 
 
     def force_collapse(self, superposition=None):
@@ -225,6 +321,8 @@ def getBlocks(nameofset):
 
     stadium = Stadium(set)
 
+    stadium.solve()
+
     return stadium.toJson()
 
 
@@ -232,4 +330,4 @@ def getBlocks(nameofset):
 
 if __name__ == '__main__':
     loadBlockSetList()
-    app.run(debug=True)
+    #app.run(debug=True)
