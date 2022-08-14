f5df8881f50b2909db713d856970c2637fca11f3
ATN04 <95452323+EtienneK2004@users.noreply.github.com>
2022-08-14 13:41:37 +0200

    API version
    This version launchs the API when run

--- main.py
+++ main.py
@@ -433,4 +433,4 @@ def getBlocks(nameofset):
 
 if __name__ == '__main__':
     loadBlockSetList()
-    # app.run(host='127.0.0.1', port=8080,debug=True)
+    app.run(host='127.0.0.1', port=8080,debug=True)
